b371926d01cce05ac54d0ee98061642bbbbbc0c3
Timmy Cho <chotimmysf@gmail.com>
2020-05-09 11:10:36 -0700

    created form submission

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -7,9 +7,19 @@ class DocsController < ApplicationController
     end
 
     def new
+        @doc = Doc.new
     end
 
     def create
+        @doc = Doc.new(doc_params)
+
+        if @doc.save
+            redirect_to @doc
+            #Refreshes the HTTPS file. Lose all content entered.
+        else
+            render 'new'
+            #Second chance in saving content. Does not refresh.
+        end
     end
 
     def edit
@@ -27,5 +37,6 @@ class DocsController < ApplicationController
     end
 
     def doc_params
+        params.require(:doc).permit(:title, :content)
     end
 end

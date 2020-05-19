9679b442ef5c7e81dc759cb7d6c124e9bdabf71c
Timmy Cho <chotimmysf@gmail.com>
2020-05-19 15:40:44 -0700

    added docs views

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,5 +1,6 @@
 class DocsController < ApplicationController
  before_action :find_doc, only: [:show, :edit, :update, :destroy]
+
     def index
         @docs = Doc.all.order("created_at DESC")
     end

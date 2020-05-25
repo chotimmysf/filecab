4a83723571b7ca79b8c0d0ff9bfd604026d85ec0
Timmy Cho <chotimmysf@gmail.com>
2020-05-24 22:45:03 -0700

    nomethoderror for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :models
   resources :bubbles
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

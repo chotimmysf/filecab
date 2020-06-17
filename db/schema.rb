f1edeb614d27c0d619de212b651b18c02c0bb008
Timmy Cho <chotimmysf@gmail.com>
2020-06-16 20:57:50 -0700

    remade the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_05_27_012425) do
+
+  create_table "docs", force: :cascade do |t|
+    t.string "title"
+    t.text "context"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
 
 end
